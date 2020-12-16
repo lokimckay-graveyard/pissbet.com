68e7d309a108ef5a703382529da4378a187f8482
sneyed <sneyed@gmail.com>
2020-12-16 12:53:05 +1000

    Fix icon flash

--- src/lib/fontawesome.js
+++ src/lib/fontawesome.js
@@ -1,9 +1,12 @@
 import { library } from "@fortawesome/fontawesome-svg-core";
 import { faTwitter, faDiscord } from "@fortawesome/free-brands-svg-icons";
 import { faExternalLinkAlt } from "@fortawesome/free-solid-svg-icons";
+import { config } from "@fortawesome/fontawesome-svg-core";
+import "@fortawesome/fontawesome-svg-core/styles.css";
 
 const init = () => {
   library.add(faTwitter, faDiscord, faExternalLinkAlt);
+  config.autoAddCss = false;
 };
 
 export default init;
