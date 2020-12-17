f2b4449709dd454d96df8c3201899313a29969a9
sneyed <sneyed@gmail.com>
2020-12-17 15:46:04 +1000

    3 second interval

--- src/config.js
+++ src/config.js
@@ -1,3 +1,3 @@
 export const pissbetDiscord = "https://discord.com/invite/p2pdbNc5d4";
 export const pissmas2 = "https://smash.gg/tournament/pissmas-2/details";
-export const refreshInterval = 1000;
+export const refreshInterval = 3000;
